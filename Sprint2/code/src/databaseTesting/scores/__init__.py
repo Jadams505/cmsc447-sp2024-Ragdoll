3f4b8ba056910f523bbea69edf82cb44380692ec
TideSinger <conbomb301@gmail.com>
2024-03-28 18:57:28 -0400

    Initial database work
    like 95% there. for some reason the database just dont actually work

---
+++ Sprint2/code/src/databaseTesting/scores/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask
+
+#Reset Database by running flask --app scores init-db. 
+#THIS DELETES ALL DATA. BE CAREFUL
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'scores.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+
+    return app
---
+++ Sprint2/code/src/databaseTesting/scores/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Scores;
+DROP TABLE IF EXISTS Levels;
+
+CREATE TABLE Users (
+    name TEXT UNIQUE,
+    userID INTEGER PRIMARY KEY AUTOINCREMENT
+);
+
+CREATE TABLE Scores (
+    userID INTEGER PRIMARY KEY,
+    FOREIGN KEY (userID) REFERENCES Users(userID),
+    levelOne INTEGER,
+    levelTwo INTEGER,
+    levelThree INTEGER,
+    levelFour INTEGER,
+    levelFive INTEGER
+);
+
+CREATE TABLE Levels ( 
+    name TEXT UNIQUE NOT NULL,
+    creatorID INTEGER,
+    FOREIGN KEY (creatorID) REFERENCES Users(userID),
+    levelID INTEGER PRIMARY KEY AUTOINCREMENT,
+    levelSerialized TEXT NOT NULL
+);
