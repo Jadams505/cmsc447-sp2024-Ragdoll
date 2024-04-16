8a7177f234f4f480e0a46561898e3d11ad53ca53
Abdul2241 <100181795+Abdul2241@users.noreply.github.com>
2024-04-16 18:27:25 -0400

    Login test

---
+++ Sprint3/code/src/templates/login/login.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+import database
+
+login = Blueprint("login", __name__, url_prefix="/login")
+
+@login.route("/", methods=["GET"])
+def login():
+    return render_template("login/login.html")
+
+@login.route("/result", methods=["POST"])
+def login_result():
+    name = request.form.get("name")
+    db = database.GetDatabase()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT id FROM data WHERE name = ?", (name,))
+    record = cursor.fetchone()
+
+    if record:
+        responseHeader = "Welcome Back!"
+        responseBody = f"Hello, {name}. Your ID is {record[0]}."
+    else:
+       
+        cursor.execute("INSERT INTO data (name) VALUES (?)", (name,))
+        db.commit()
+        new_id = cursor.lastrowid
+        responseHeader = "Welcome!"
+        responseBody = f"Hello, {name}. Your new ID is {new_id}."
+
+    cursor.close()
+    return render_template(
+        "login/result.html",
+        title="Login Result",
+        responseHeader=responseHeader,
+        responseBody=responseBody,
+    )
